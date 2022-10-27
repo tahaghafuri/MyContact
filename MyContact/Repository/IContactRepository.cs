5c1a2164adcce8cc29c3e0a26db868954cf17455
tahaghafuri <tahaghafuri@gmail.com>
2022-10-27 11:14:22 +0330

    Complete The IContactRepository

--- MyContact/Repository/IContactRepository.cs
+++ MyContact/Repository/IContactRepository.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace MyContact.Repository
 {
     internal interface IContactRepository
     {
-        bool Insert(string name,string details);
+        DataTable SelectAll();
+        DataTable SelectRow(int id);
+        bool Insert(string name, string details);
+        bool Update(int id, string name, string details);
+        bool Delete(int id);
     }
 }
