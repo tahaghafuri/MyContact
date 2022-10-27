eef5b70f0e66c13b0df180645f166686e87ac1db
tahaghafuri <tahaghafuri@gmail.com>
2022-10-27 11:31:29 +0330

    Update

---
+++ MyContact/Service/ContactRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace MyContact
+{
+    class ContactRepository : IContactRepository
+    {
+        private string connection = "Data Source=.;Initial Catalog=MyContact;Integrated Security=true";
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(string name, string details)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable SelectAll()
+        {
+            string query = "Select * From MyContact";
+            SqlConnection conn = new SqlConnection(connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(query,conn);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            return data;
+        }
+
+        public DataTable SelectRow(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, string name, string details)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
