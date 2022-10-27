5e9711ffa2be72b2781154bfe2f3442ea979aad5
tahag <tahag@PC>
2022-10-27 10:56:55 +0330

    Add project files.

---
+++ MyContact/Repository/IContactRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyContact.Repository
+{
+    internal interface IContactRepository
+    {
+        bool Insert(string name,string details);
+    }
+}
